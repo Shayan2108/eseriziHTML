aad17c5d9d1f1fb675cbe904f8f33dcf5ae20ffc
ciao <ciao>
2026-03-18 13:43:42 +0100

    aggiunta bellisime cose

--- esercizio3/script.js
+++ esercizio3/script.js
@@ -1,2 +1,10 @@
-var ciao = 100;
-console.log("bella bro io sono balutte " + ciao);
+function generaElenco() {
+  let s = "";
+  s += "<ul>\n";
+  for (let i = 0; i < 10; i++) {
+    s += "<li> item numero: " + i + "<li>\n";
+  }
+  s += "</ul>\n";
+  console.log(s)
+}
+generaElenco();
